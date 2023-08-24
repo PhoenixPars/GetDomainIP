eeff22af0e66850dba81fa526856883986e25df2
PhoenixPars <60186819+PhoenixPars@users.noreply.github.com>
2023-08-24 10:55:00 +0330

    Update Get.sh

--- Get.sh
+++ Get.sh
@@ -1,9 +1,17 @@
+#!/bin/bash
 apt install python -y
-apt install ncurses-utils
+
 echo "———————————————————————————"
 echo "| 1 . show my host name   |"
 echo "| 2 . Get Domain Ip       |"
 echo "———————————————————————————"
 echo " t.me/PhoenixPars          "      
 echo "———————————————————————————"
-input -p " What tool do you want to use [1/2] ? " inputtool
+read -p " What tool do you want to use [1/2] ? " inputtool
+
+if [["${inputtool}" == "1"]]; then
+bash <(curl -Ls https://raw.githubusercontent.com/PhoenixPars/ShowMyIp/main/Show.sh)
+elif [["${inputtool}" == "2"]]; then
+
+else
+read -p " Select a tool from the menu [1/2] ! " inputtool
