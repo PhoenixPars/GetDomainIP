b80a1bd65dd0afc123f3fda81eff65566912b6bc
PhoenixPars <60186819+PhoenixPars@users.noreply.github.com>
2023-08-24 11:04:29 +0330

    Update Get.sh

--- Get.sh
+++ Get.sh
@@ -9,9 +9,9 @@ echo " t.me/PhoenixPars          "
 echo "———————————————————————————"
 read -p " What tool do you want to use [1/2] ? " inputtool
 
-if [["${inputtool}" == "1"]]; then
+if [["${inputtool}" == 1]]; then
 bash <(curl -Ls https://raw.githubusercontent.com/PhoenixPars/ShowMyIp/main/Show.py)
-elif [["${inputtool}" == "2"]]; then
+elif [["${inputtool}" == 2]]; then
 bash <(curl -Ls https://raw.githubusercontent.com/PhoenixPars/GetDomainIP/main/Get.py)
 else
 echo " Select a tool from the menu [1/2] ! "
