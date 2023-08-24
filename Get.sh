0e6e3f19b1bd22f4a3837b81a6f849d50d61e63a
PhoenixPars <60186819+PhoenixPars@users.noreply.github.com>
2023-08-24 10:48:11 +0330

    Update Get.sh

--- Get.sh
+++ Get.sh
@@ -1,1 +1,9 @@
 apt install python -y
+apt install ncurses-utils
+echo "———————————————————————————"
+echo "| 1 . show my host name   |"
+echo "| 2 . Get Domain Ip       |"
+echo "———————————————————————————"
+echo " t.me/PhoenixPars          "      
+echo "———————————————————————————"
+input -p " What tool do you want to use [1/2] ? " inputtool
